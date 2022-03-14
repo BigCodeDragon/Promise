7d61feb5683d839c462c45b5cfa5e7a3818aa7f3
CodeDragon <1505207242@qq.com>
2022-03-15 00:27:11 +0800

    feat: 完成基本结构

--- Promise.js
+++ Promise.js
@@ -1,5 +1,37 @@
-class Promise {
+class MyPromise {
+  static PENDING = "pending";
+  static FUllFILLED = "fullfilled";
+  static REJECT = "reject";
+  constructor(fn) {
+    this.result = null;
+    this.status = MyPromise.PENDING;
+    fn(this.resolve.bind(this), this.reject.bind(this));
+  }
 
+  resolve(result) {
+    if (this.status === MyPromise.PENDING) {
+      this.result = result;
+      this.status = MyPromise.FUllFILLED;
+    }
+  }
+  reject(result) {
+    if (this.status === MyPromise.PENDING) {
+      this.result = result;
+      this.status = MyPromise.REJECT;
+    }
+  }
+  then(onFullFilled, onReject) {
+    
+    if (this.status === MyPromise.FUllFILLED) {
+      onFullFilled(this.result);
+    }
+    if (this.status === MyPromise.REJECT) {
+      onReject(this.result);
+    }
+  }
 }
 
-const promise = new Promise()
+const promise = new MyPromise((resolve, reject) => {
+  resolve(123);
+});
+promise.then((result) => console.log(result));
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Promise
 手写Promise
+1 初始结构
+2 this指向
+3 then
+4 执行异常
+5 异步
+6 回调保存
+7 链式调用
